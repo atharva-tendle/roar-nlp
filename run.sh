a4a9a8eceb2791c131cf6e9e8724dd7a813d31fb
atharva-tendle <atendle13.3.98@gmail.com>
2020-10-16 10:14:31 -0500

    Added run file

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --time=168:00:00
+#SBATCH --partition=gpu
+#SBATCH --mem=64gb
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --constraint='gpu_32gb&gpu_v100'
+#SBATCH --job-name=baseline
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=atendle13.3.98@gmail.com
+#SBATCH --output=/work/cse896/atendle/out-files/imdb-baseline.out
+
+export HF_DATASETS_CACHE=/work/cse896/atendle/.cache/datasets/
+export TRANSFORMERS_CACHE=/work/cse896/atendle/.cache/transformers/
+export HF_MODULES_CACHE=/work/cse896/atendle/.cache/huggingface/
+module load anaconda
+conda activate /work/vinod/gwirka/.conda/envs/nlp-pbi
+python -u $@ baseline.py --epochs 10 --dataset IMDb
